b167e52fa5085e533108b57e8dc3e77239002fd1
Carlos Matthews <cromaticofilms@gmail.com>
2020-08-25 12:42:52 -0300

    commit inicial

---
+++ ajedrez.py
@@ -0,0 +1,135 @@
+# tipos de piezas:
+REY = "R"
+REINA = "RA"
+CABALLO = "C"
+ALFIL = "A"
+TORRE = "T"
+PEON = "P"
+
+# colores de piezas:
+BLANCO = "B"
+NEGRO = "N"
+
+def crear_tablero():
+    tablero = {}
+    tablero[(0,0)] = (TORRE,    BLANCO)
+    tablero[(0,1)] = (CABALLO,  BLANCO)
+    tablero[(0,2)] = (ALFIL,    BLANCO) 
+    tablero[(0,3)] = (REINA,    BLANCO)
+    tablero[(0,4)] = (REY,      BLANCO)
+    tablero[(0,5)] = (ALFIL,    BLANCO)
+    tablero[(0,6)] = (CABALLO,  BLANCO)
+    tablero[(0,7)] = (TORRE,    BLANCO)
+    for i in range(8):
+        tablero[1,i] = (PEON, BLANCO)
+    tablero[(7,0)] = (TORRE,    NEGRO)
+    tablero[(7,1)] = (CABALLO,  NEGRO)
+    tablero[(7,2)] = (ALFIL,    NEGRO) 
+    tablero[(7,3)] = (REINA,    NEGRO)
+    tablero[(7,4)] = (REY,      NEGRO)
+    tablero[(7,5)] = (ALFIL,    NEGRO)
+    tablero[(7,6)] = (CABALLO,  NEGRO)
+    tablero[(7,7)] = (TORRE,    NEGRO)
+    for i in range(8):
+        tablero[6,i] = (PEON, NEGRO)
+    return tablero    
+
+def representacion_piezas(nombre_pieza):
+   # https://qwerty.dev/chess-symbols-to-copy-and-paste/
+    if nombre_pieza is None:
+        return " "
+    
+    convercion = {    
+        (TORRE,    BLANCO):"♖",
+        (CABALLO,  BLANCO):"♘",
+        (ALFIL,    BLANCO):"♗",
+        (REINA,    BLANCO):"♕",
+        (REY,      BLANCO):"♔",
+        (PEON,     BLANCO):"♙",
+        (TORRE,    NEGRO):"♜",
+        (CABALLO,  NEGRO):"♞",
+        (ALFIL,    NEGRO):"♝",
+        (REINA,    NEGRO):"♛",
+        (REY,      NEGRO):"♚",
+        (PEON,     NEGRO):"♟"
+    }
+    return convercion[nombre_pieza]
+
+def chequear_movimiento(tablero,posicion1,posicion2,turno_de): 
+    #revisa que alla un pieza en casillero a mover
+    if posicion1 not in tablero:
+        print("no hay pieza en el casillero")
+        return False 
+       
+    pieza_ataque = tablero[posicion1]
+    color_ataque = pieza_ataque[1] 
+    #revisa si hay pieza en casillero destino y si es del oponente
+    if posicion2 in tablero:
+        pieza_destino = tablero.get((posicion2))
+        color_destino = pieza_destino[1] #aca indice para obtener color
+        if color_ataque == color_destino:
+            print("no puede comer su propia pieza")
+            return False
+    #revisa si eljijio pieza de acuerdo al color del turno
+    if turno_de != color_ataque:
+        print("elija un pieza del color",color_ataque)         
+        return False
+    
+    return True
+
+
+
+
+def mover(tablero, posicion1, posicion2):
+    #chequear = chequear_movimiento(tablero,posicion1,posicion2)  
+    #if chequear : 
+    pieza = tablero.get(posicion1)
+    tablero[posicion2] = pieza
+    del tablero[posicion1] 
+    #if not chequear:
+        #print("movimiento invalido, vuelve a introducir jugada")
+       
+    return tablero 
+
+    
+        
+        
+    
+    
+    
+# hacer un main que te vaya pidiendo 2 posiciones, y haga el movimiento
+
+
+""" def come_a(tipo_pieza, posicion, posicion2):
+    # come_a(TORRE, (0,0), (1,0)) -> True
+    # come_a(PEON, (1,2), (2,2)) -> False
+    # come_a(PEON, (1,2), (2,3)) -> True
+    # come_a(PEON, (1,2), (2,3)) -> True
+    pass """
+
+
+
+def print_tablero(tablero):  
+    for n in range(8):
+        print("  ",n,"",end="",)
+    print()        
+    for fila in range(8):
+        print(fila," ——— " * 8)       
+  
+        for col in range(8):
+            pieza = tablero.get((fila,col))
+            simbolo = representacion_piezas(pieza)
+            print(" |", simbolo, end=' ')
+        print(" |")    
+    print("  ———" * 8)
+
+
+    
+    
+
+
+    
+
+
+    
+
---
+++ piezas_ajedrez.py
@@ -0,0 +1,153 @@
+from ajedrez import representacion_piezas
+from ajedrez import print_tablero
+#####esta parte despues se borrar es para facilitar mientras se escribe######
+# tipos de piezas:
+REY = "R"
+REINA = "RA"
+CABALLO = "C"
+ALFIL = "A"
+TORRE = "T"
+PEON = "P"
+
+# colores de piezas:
+BLANCO = "B"
+NEGRO = "N"
+
+def crear_tablero():
+    tablero = {}
+    tablero[(0,0)] = (TORRE,    BLANCO)
+    tablero[(0,1)] = (CABALLO,  BLANCO)
+    tablero[(0,2)] = (ALFIL,    BLANCO) 
+    tablero[(0,3)] = (REINA,    BLANCO)
+    tablero[(0,4)] = (REY,      BLANCO)
+    tablero[(0,5)] = (ALFIL,    BLANCO)
+    tablero[(0,6)] = (CABALLO,  BLANCO)
+    tablero[(0,7)] = (TORRE,    BLANCO)
+    for i in range(8):
+        tablero[1,i] = (PEON, BLANCO)
+    tablero[(7,0)] = (TORRE,    NEGRO)
+    tablero[(7,1)] = (CABALLO,  NEGRO)
+    tablero[(7,2)] = (ALFIL,    NEGRO) 
+    tablero[(7,3)] = (REINA,    NEGRO)
+    tablero[(7,4)] = (REY,      NEGRO)
+    tablero[(7,5)] = (ALFIL,    NEGRO)
+    tablero[(7,6)] = (CABALLO,  NEGRO)
+    tablero[(7,7)] = (TORRE,    NEGRO)
+    for i in range(8):
+        tablero[6,i] = (PEON, NEGRO)
+    return tablero    
+
+
+
+
+    
+    
+
+def obtner_pieza(tablero, posicion):
+    pieza = tablero.get(posicion)
+    tipo_de_pieza = pieza[0]
+    color_pieza = pieza[1]
+    return tipo_de_pieza, color_pieza 
+    
+    
+    
+def mover_peon(posicion):
+    mov_1 = (posicion[0]+1,posicion[1])   
+    mov_2 = (posicion[0]-1,posicion[1])
+
+    movimientos_posibles = [mov_1,mov_2]   
+    return movimientos_posibles
+
+def mover_caballo(posicion):
+    fila = posicion[0]
+    columna = posicion[1]
+    mov_1 = (fila -2, columna -1)
+    mov_2 = (fila -2, columna +1)
+    mov_3 = (fila -1, columna -2)
+    mov_4 = (fila -1, columna +2)
+    mov_5 = (fila +1, columna -2)
+    mov_6 = (fila +1,columna  +2)
+    mov_7 = (fila +2, columna -1)
+    mov_8 = (fila +2, columna +1)
+    
+    movimientos_posibles = [mov_1,mov_2,mov_3,mov_4,mov_5,mov_6,mov_7,mov_8]
+    
+    return movimientos_posibles
+
+def mover_torre(posicion):
+    fila = posicion[0]
+    columna = posicion[1]
+    movimientos_posibles= []
+    
+    for n  in range(8):
+        movimientos_posibles.append((n,columna))
+        movimientos_posibles.append((fila,n))
+    indice_posicion_actual = movimientos_posibles.index(posicion)
+    del movimientos_posibles[indice_posicion_actual]
+    indice_posicion_actual = movimientos_posibles.index(posicion)
+    del movimientos_posibles[indice_posicion_actual]
+
+    return movimientos_posibles
+
+def mover_alfil(posicion):
+    fila = posicion[0]
+    columna = posicion[1]
+    movimientos_posibles = []
+    rango = fila +1 
+    for n in range(1,rango):
+        movimientos_posibles.append((fila-n,columna-n))
+        movimientos_posibles.append((fila-n,columna+n))
+    
+    rango_inverso = 7-fila
+    for n in range (1,rango_inverso+1):
+        movimientos_posibles.append((fila+n,columna-n)) 
+        movimientos_posibles.append((fila+n,columna+n))
+    
+    #filtrado de posicion no validas
+    for e in movimientos_posibles:
+        if e[0] > 7 or e[1] > 7 :
+            posicion_nula = movimientos_posibles.index(e)
+            del movimientos_posibles[posicion_nula]
+        if e[0] < 0 or e[1] < 0:
+            posicion_nula = movimientos_posibles.index(e)
+            del movimientos_posibles[posicion_nula]
+    
+    print(movimientos_posibles)
+    return movimientos_posibles
+
+# hacer funciones faltantes
+def mover_rey(posicion):
+   return print("todo rey")
+def mover_reina(posicion):
+    return print("todo reina")
+
+
+dicc_movimientos ={
+    PEON : mover_peon,
+    TORRE : mover_torre,
+    CABALLO : mover_caballo,
+    ALFIL : mover_alfil,
+    REINA : mover_reina,
+    REY : mover_rey
+    }
+
+
+#esta funcion es solo para probar las otras...grafica dado una posicion de pieza de inicio.
+#y la funcion q le damos segun la íeza a probar
+def probar_mov(posicion, funcion_a_probar):
+
+    movimientos = funcion_a_probar(posicion)
+    
+    tablero = {(posicion):(REY,NEGRO)}
+    for tupla in movimientos:
+        tablero[tupla]=(PEON,BLANCO)
+
+    print_tablero(tablero)
+
+
+posicion = (2,3)
+
+b = a(2,3)
+print(b)
+
+
