ce04f7b13295d41eb5642151039dc551b351c7e9
Martín Massera <martinmassera@gmail.com>
2020-08-25 13:02:49 -0300

    borre el comentario

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-# este es el main 
-
 from ajedrez import *
 
 tablero_inicio = crear_tablero()
@@ -15,7 +13,7 @@ while True:
         jugador = BLANCO
     else:
         jugador = NEGRO
-    
+
     print("turno de jugador",jugador)
     posicion1 = int(input("Posicion de la pieza a mover(fila/columna): "))
     posicion2 = int(input("Posicion de destino de la pieza(fila/columna): "))
@@ -23,7 +21,7 @@ while True:
     posicion2 = (posicion2 // 10),(posicion2 % 10 )
     if jugada == 0 :
         tablero = tablero_inicio
-    
+
     if chequear_movimiento(tablero,posicion1,posicion2,jugador):
         tablero = mover(tablero,posicion1,posicion2)
         print_tablero(tablero)
