5b7891cc314d4da35a93a67787040fb2d62a5e68
Carlos Matthews <cromaticofilms@gmail.com>
2020-09-04 20:33:38 -0300

    funcion mover peon

--- piezas_ajedrez.py
+++ piezas_ajedrez.py
@@ -18,7 +18,7 @@ def crear_tablero():
     tablero = {}
     tablero[(0,0)] = (TORRE,    BLANCO)
     tablero[(0,1)] = (CABALLO,  BLANCO)
-    tablero[(5,2)] = (ALFIL,    BLANCO) ####corrido para pruebaaaaaaaa va en (0,2)
+    tablero[(0,2)] = (ALFIL,    BLANCO) 
     tablero[(0,3)] = (REINA,    BLANCO)
     tablero[(0,4)] = (REY,      BLANCO)
     tablero[(0,5)] = (ALFIL,    BLANCO)
@@ -49,7 +49,6 @@ def obtner_pieza(tablero, posicion):
 def casillero_esta_libre(tablero,posicion):
     if posicion not in tablero:
         return True
-
 
 def limites_del_tablero(posicion):
     return posicion[0] >= 0 and posicion[0] <= 7 and posicion[1] >=0 and posicion[1] <= 7
@@ -92,14 +91,44 @@ def generar_posiciones_posibles(tablero, posicion, direccion):
     return movimientos_posibles
 
 
-def mover_peon(posicion):
+def mover_peon(tablero, posicion): # TODO: moviento 2 casiileros 1er jugada
+    movimientos_posibles = []
+    turno = obtner_pieza(tablero,posicion)
+    turno = turno[1]
+    print(turno)
     
-    mov_1 = (posicion[0]+1,posicion[1])
-    mov_2 = (posicion[0]-1,posicion[1])
-    if limites_del_tablero(mov_1) and limites_del_tablero(mov_2):
-        movimientos_posibles = [mov_1 ,mov_2]
+    #movientos posibles 1 para adelante
+    def uno_adelante(tablero, posicion):
+        if limites_del_tablero(posicion) and casillero_esta_libre(tablero, posicion):
+            movimientos_posibles.append(posicion)
+    #movimientos posibles comer/diagonal
+    def pos_comer_diag(tablero,posicion1, posicion2):
+        if not casillero_esta_libre(tablero, posicion2) :
+            if la_pieza_es_oponente(tablero, posicion1, mov_3):
+                movimientos_posibles.append(posicion2) 
 
+    #blancas hacia adelante
+    mov_blancas = (posicion[0]+1,posicion[1]) 
+    #negras hacia adelante
+    mov_negras = (posicion[0]-1,posicion[1])  
     
+    #para blancas diagonal
+    mov_3 = (posicion[0]+1,posicion[1]-1)
+    mov_4 =(posicion[0]+1,posicion[1]+1)
+    #para negras diagonal
+    mov_5 = (posicion[0]-1,posicion[1]-1)
+    mov_6 = (posicion[0]-1,posicion[1]+1)
+    
+    if turno == BLANCO: 
+        uno_adelante(tablero,mov_blancas)
+        pos_comer_diag(tablero,posicion,mov_3)
+        pos_comer_diag(tablero,posicion,mov_4)
+    
+    else:
+        uno_adelante(tablero,mov_negras)  
+        pos_comer_diag(tablero,posicion,mov_5)
+        pos_comer_diag(tablero,posicion,mov_6)   
+         
     return movimientos_posibles
 
 
