77861b135d7f153bffe82cf2befca2382ad4bd3e
Carlos Matthews <cromaticofilms@gmail.com>
2020-10-30 14:56:19 -0300

    prueva server

--- server.py
+++ server.py
@@ -10,7 +10,28 @@ app = Flask(__name__, static_url_path="/frontend")
 
 @app.route('/')
 def index():
-    return 'Hello world'
+    return 'Hello'
+
+
+@app.route('/carli')
+def carli():
+    return 'Hello carli'
+
+@app.route('/marto')
+def marto():
+    return 'Hello marto'
+
+@app.route('/dicc')
+def dicc():
+    diccionario = {}
+    diccionario["cero"]= 0
+    diccionario["uno"] = 1
+    diccionario["dos"] = 2
+
+    return json.dumps(diccionario)    
+    
+    
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8000)
