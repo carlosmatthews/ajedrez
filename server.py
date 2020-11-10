0b3f9a07cf637cf84a67fc780c4571d6374cb5d3
Carlos Matthews <cromaticofilms@gmail.com>
2020-11-10 18:11:38 -0300

    correcion server

--- server.py
+++ server.py
@@ -7,26 +7,34 @@ import json
 from flask import Flask
 from flask import request
 from manejo_partidas import serializar_tablero
-from ajedrez import*
+from ajedrez import partida, crear_tablero, chequear_movimiento_1,chequear_movimiento_2, movimientos_posibles
 import piezas_ajedrez
 
 app = Flask(__name__, static_url_path="/frontend")
 
+tablero = partida["tablero"]
+jugador = partida["turno"]
+
+dic_error = {"error": "error en la ejecucion del movimiento"}
+
+
 @app.route("/inicio")
+#borrar
 def inicio():
+    global tablero
     tablero = crear_tablero()
-    tablero = serializar_tablero(tablero)
-    return json.dumps(tablero)
+    tablero_serializado = serializar_tablero(tablero)
+    return json.dumps(tablero_serializado)
 
 
 
-#?fila=0&col=1
+#/movimientos?fila=0&col=1
 @app.route("/movimientos")     
 def movimientos():
     fila = request.args.get("fila")
     col = request.args.get("col")
     if chequear_movimiento_1(tablero,(fila,col),jugador) == False:
-        return json.dumps("error")
+        return json.dumps(dic_error)
     else:
         return json.dumps(movimientos_posibles)
 
@@ -34,11 +42,10 @@ def movimientos():
 
 @app.route('/tablero')
 def enviar_tablero():
-    tablero = tablero
     tablero_serializado = serializar_tablero(tablero)
     tablero_json = json.dumps(tablero_serializado)
-
     return tablero_json
+
 #parametros  #?fila=0,col=0&fila2=5,col2=4')
 @app.route("/mover")        
 def mover():
@@ -46,13 +53,13 @@ def mover():
     col = request.args.get ("col")
     fila2 = request.args.get("fila2")
     col2 = request.args.get("col2")
-    try:
-        piezas_ajedrez.mover(tablero,(fila,col),(fila2,col2))
-        tablero_serializado = serializar_tablero(tablero)
-        return json.dumps(tablero_serializado) 
 
-    except:
-        return json.dumps("error en la ejecucion del movimiento")
+    piezas_ajedrez.mover(tablero,(fila,col),(fila2,col2))
+    tablero_serializado = serializar_tablero(tablero)
+    return json.dumps(tablero_serializado) 
+
+    
+      
     
     
 
