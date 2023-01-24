3172c6dd8151c77a49566c005a1608d9236171c0
Martin Massera <martinmassera@gmail.com>
2023-01-24 16:14:50 -0300

    including build in heroku branch

---
+++ frontend/build/precache-manifest.456893097805b0b995b8b932b254592d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5900d5d9f025667a374d2102f7d1414c",
+    "url": "/frontend/index.html"
+  },
+  {
+    "revision": "2a17dee98e8d25cf0f80",
+    "url": "/frontend/static/css/main.7937738d.chunk.css"
+  },
+  {
+    "revision": "9c29fb8dba56ef91d103",
+    "url": "/frontend/static/js/2.9e5c995c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/frontend/static/js/2.9e5c995c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a17dee98e8d25cf0f80",
+    "url": "/frontend/static/js/main.378cbc2d.chunk.js"
+  },
+  {
+    "revision": "4853b67471137a6193d2",
+    "url": "/frontend/static/js/runtime-main.6b7e4e23.js"
+  }
+]);
